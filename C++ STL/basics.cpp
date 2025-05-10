dd70be7d0d65500acb050a227865719f75a99af2
acinaction <chauhanarsh1312@gmail.com>
2025-05-10 19:51:51 +0530

    Started with C++ STL basis

---
+++ C++ STL/basics.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+// C++ STL is divided into 4 major parts:
+// 1. Containers
+//    - Sequencial Containers: Vectos, Stack, Queue, Pair;
+//    - Ordered Containers: Maps, Multimaps, Sets, Multisets;
+//    - Unordered Containers: Unordered Maps, Unordered Sets;
+
+//    Nested Containers:
+//    - Vectors of vectors
+//    - Maps of vectors
+//    - Vectors of maps
+//    - Sets of Pairs
+
+// 2. Iterators
+//    - Iterators are used to point to the memory addresses of STL containers.
+//    - begin() and end() functions are used to get the first and last elements of the container.
+//    - Continuity for Containers: The iterators of the containers are continuous.
+
+// 3. Algorithms
+//    - Algorithms are used to perform operations on the containers.
+//    - Algorithms are defined in the <algorithm> header file.
+//    - Algorithms are used to perform operations on the containers.
+
+// 4. Functors
+//    - Functors are classes that can be called as if they are functions.
+int main(){
+
+}
