b2527879955b4936a0e291873bdf6f4b39f63098
acinaction <chauhanarsh1312@gmail.com>
2025-05-13 07:42:34 +0530

    Theory 7 started

---
+++ C++ STL/Theory7.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+// Nesting of sets and maps
+int main(){
+    // Sorting in pairs
+    // If first element is small then pair is small
+    // If first element is equal then decide by second 
+
+    // Similar thing happens in sets and vectors
+    
+    map<pair<string, string>, vector<int>> m;
+    // Example we have first name last name as pair and marks as vector
+    int n;
+    cin>>n;
+    while(n--){
+        pair<string,string> p;
+        cin>>
+    }
+}
